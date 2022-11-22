18fe7410f8d470b61480dfc32c4108dac5f11327
José Rogério Filho <roger.sama@gmail.com>
2022-11-22 21:42:24 +0100

    Adds material-ui as dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { styled } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      maxWidth="md"
+      sx={{
+        height: '100%'
+      }}
+    >
+      <Grid
+        container
+        spacing={4}
+        alignContent="center"
+        sx={{
+          height: '100%'
+        }}
+      >
+        <Grid item xs={12} sm={8}>
+          <Item elevation={4}>xs=8</Item>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Item elevation={4}>xs=4</Item>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Item elevation={4}>xs=4</Item>
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <Item elevation={4}>xs=8</Item>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
